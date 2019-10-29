33442d69a99d186b2c0e41e69749c7924b3e5676
aleibo92 <aleibo92@gmail.com>
2019-10-29 07:20:40 +0000

    commit 2

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,2 +1,20 @@
 class TestsController < ApplicationController
+
+  def index
+    @tests = Test.all
+  end
+  def new
+    @test = Test.new
+  end 
+  def create
+    @test = Test.create(test_params)
+    redirect_to tests_path
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:question, :answer)
+  end
+
 end
